a277750d11e0527914fcf2a0457d8cccc6b35366
eric tsai <esytsai@berkeley.edu>
2017-11-18 14:56:55 -0800

    layouts done

--- frontend/solar-frontend/src/containers/homePage.js
+++ frontend/solar-frontend/src/containers/homePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
-import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
 
@@ -14,23 +14,36 @@ class HomePage extends React.Component {
     ,{"name":"test2", "location":"location2"},{"name":"test2", "location":"location2"},{"name":"test2", "location":"location2"}]
     return(
       <div style={{ marginTop: 10 }}>
-        <Grid sm={6}>
-        <div style={{overflow: 'scroll', height: 700 }}>
-          {data.map(function(d, idx){
-            return (
-                <Card key={idx} className={classes.card}>
-                  <CardContent>
-                    <Typography type="body1" className={classes.title}>
-                      {d.name} {d.location}
-                    </Typography>
-                  </CardContent>
-                </Card>
-            )
-          })}
-        </div>  
-        </Grid>
-        <Grid sm={6}>
-          <div />
+        <Grid container>
+          <Grid item sm={6}>
+          <div style={{overflow: 'scroll', height: 700 }}>
+            {data.map(function(d, idx){
+              return (
+                  <Card key={idx} className={classes.card}>
+                    <CardContent>
+                      <Typography type="body1" className={classes.title}>
+                        {d.name} {d.location}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+              )
+            })}
+          </div>  
+          </Grid>
+          <Grid item sm={6}>
+            <Card>
+            <CardMedia
+              className={classes.media}
+              image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg/280px-Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography type="headline" component="h2">
+                Lizard
+              </Typography>
+            </CardContent>
+            </Card>
+          </Grid>
         </Grid>
       </div>
     )
@@ -55,6 +68,9 @@ const styles = theme => ({
     marginBottom: 12,
     color: theme.palette.text.secondary,
   },
+  media: {
+    height: 600
+  }
 });
 
 
