3ddc63bc5bfb5d7e2253f489c84888f3b2fd2bf5
eric tsai <esytsai@berkeley.edu>
2017-11-18 16:46:55 -0800

    listings done

--- frontend/solar-frontend/src/containers/homePage.js
+++ frontend/solar-frontend/src/containers/homePage.js
@@ -9,11 +9,11 @@ class HomePage extends React.Component {
 
   render () {
     const { classes } = this.props
-    const data =[{"name":"Apartment rooftop in South SF, CA", "location":"location1", "size": "size1", "completion": "Comp1", "ret":"ret1", "img":"https://dl6m636cbz9dr.cloudfront.net/2016/08/Millet-Site_Land-Lease-Blog.jpg"},
-    {"name":"House roof in Downtown Berkeley, CA", "location":"location1", "size": "size1", "completion": "Comp1", "ret":"ret1", "img":"img1"},
-    {"name":"Residential rooftop in West Park, Fresno, CA", "location":"location1", "size": "size1", "completion": "Comp1", "ret":"ret1", "img":"img1"},
-    {"name":"test1", "location":"location1", "size": "size1", "completion": "Comp1", "ret":"ret1", "img":"img1"},
-    {"name":"test1", "location":"location1", "size": "size1", "completion": "Comp1", "ret":"ret1", "img":"img1"},]
+    const data =[{"name":"Apartment rooftop in South SF, CA", "location":"20.3 miles", "size": "474", "completion": "70% completed ($1050 raised out of $1500), $450 left", "ret":"$0.05", "img":"https://i.pinimg.com/736x/83/47/24/8347246fdc9a390fc9fb6d574c1f1dcb--roofing-options-best-roofing.jpg "},
+    {"name":"House roof in Downtown Berkeley, CA", "location":"0.9 mile", "size": "370", "completion": "30% completed ($360 raised out of $1200), $450 left", "ret":"$0.07", "img":"http://www.dreamgreenhomes.com/plans/images/green-houseP.jpg"},
+    {"name":"Residential rooftop in West Park, Fresno, CA", "location":"186 miles", "size": "240", "completion": "50% completed, ($450 raised out of $900), $450 left", "ret":"$0.03", "img":"http://www.everbluetraining.com/sites/default/files/rooftop-solar-panels.jpg"},
+    {"name":"Large apartment roof in Salinas, CA", "location":"107 miles", "size": "1427", "completion": "95% completed, ($2850 raised out of $3000), $150 left", "ret":"$0.12", "img":"https://justinianorealestaterenovations.files.wordpress.com/2015/06/roof-after.jpg"},
+    {"name":"Flat roof in Orchard, Davis, CA", "location":"73.6 miles", "size": "592", "completion": "86% completed, ($1634 raised out of $1900), $266 left", "ret":"$0.09", "img":"https://www.ucdavis.edu/sites/default/files/image_gallery/20131204_green_roof_9055.jpg"},]
     return(
       <div style={{ marginTop: 10 }}>
         <Grid container>
@@ -35,7 +35,10 @@ class HomePage extends React.Component {
                         {d.size} sq. ft., Distance: {d.location}
                       </Typography>
                       <Typography type="body1" className={classes.title}>
-                        ${d.completion} Raised, {d.ret} estimated monthly return per dollar
+                        {d.completion}
+                      </Typography>
+                      <Typography type="body1" className={classes.title}>
+                        {d.ret} estimated monthly return per dollar
                       </Typography>
                       </Grid>
                       <Grid item sm={1}>
@@ -86,7 +89,6 @@ const styles = theme => ({
   title: {
     marginBottom: 16,
     fontSize: 14,
-    color: theme.palette.text.secondary,
   },
   pos: {
     marginBottom: 12,
